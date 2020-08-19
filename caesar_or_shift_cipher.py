8472d62cd8421b4d034f80206430a85e3f2ecc4d
VikyGk <53996859+VikyGk@users.noreply.github.com>
2020-08-19 14:23:20 +0530

    Add files via upload

---
+++ caesar_or_shift_cipher.py
@@ -0,0 +1,34 @@
+def encrypt(text,s, decip): 
+    result = "" 
+  
+    # traverse text 
+    for i in range(len(text)): 
+        char = text[i] 
+  
+        # Encrypt uppercase characters 
+        if (char.isupper()): 
+            if decip:
+                result +=  chr((ord(char) - s-65) % 26 + 65)    
+            else:
+                result +=  chr((ord(char) + s-65) % 26 + 65)    
+            
+            
+  
+        # Encrypt lowercase characters 
+        elif(char.islower()): 
+            if decip:
+                result += chr((ord(char) - s - 97) % 26 + 97)
+            else:
+                result += chr((ord(char) + s - 97) % 26 + 97)
+        else:
+            result += char
+  
+    return result 
+  
+#check the above function 
+text = "phhw ph diwhu wkh wrjd sduwb"
+s = 3
+decip = True
+print("Text  : ",text )
+print("Shift : ",str(s)) 
+print("Cipher: ",encrypt(text,s, decip))
